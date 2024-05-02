8c14ae7c0091f645ba205d4060c3701d2d163f94
D0V4H-K11N <ledio.duda@gmail.com>
2024-05-02 18:35:23 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+// Parses the JSON returned by a network request
+const parseJSON = (resp) => (resp.json ? resp.json() : resp);
+
+// Checks if a network request came back fine, and throws an error if not
+const checkStatus = (resp) => {
+  if (resp.status >= 200 && resp.status < 300) {
+    return resp;
+  }
+
+  return parseJSON(resp).then((resp) => {
+    throw resp;
+  });
+};
+
+const headers = { "Content-Type": "application/json" };
 
 function App() {
+  const [error, setError] = useState(null);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:1337/api/items?populate=*", {
+      headers,
+      method: "GET",
+    })
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(({ data }) => setItems(data))
+      .catch((error) => setError(error));
+  }, []);
+
+  if (error) {
+    // Print errors if any
+    return <div>An error occurred: {error.message}</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Items</h1>
+        <ul>
+          {items.map((item) => {
+            // Log the URL to the console
+            console.log(
+              "Image URL:",
+              item.attributes.Profile.data.attributes.url
+            );
+
+            return (
+              <li key={item.id}>
+                <h2>{item.attributes.Name}</h2>
+                {item.attributes.Profile && (
+                  <img
+                    src={`http://localhost:1337${item.attributes.Profile.data.attributes.url}`}
+                    alt={item.attributes.Name}
+                  />
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
