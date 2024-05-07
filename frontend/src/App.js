eacf178be682a4de7d3616eff55f5e97f83cd916
sofro21 <andreisofronie25@gmail.com>
2024-05-07 14:12:34 +0200

    commits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,14 @@ import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import FAQ from './Components/FAQ/FAQ';
 import ServiceSection from './Components/Service-Section/ServiceSection';
+import Board_of_Directors from './Components/Board_of_Directors/Board_of_Directors';
+import ShowBoardofDirectors from './Components/Board_of_Directors/Board_of_Directors';
 
 function App() {
   return (
     <>
       <Navbar/>
+      <ShowBoardofDirectors/>
       <ServiceSection/>
       <FAQ/>
       <Footer/>
