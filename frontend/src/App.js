b9f4d56df5cc539d9c1a893cca37bde6fef126e0
D0V4H-K11N <ledio.duda@gmail.com>
2024-06-15 18:41:00 +0200

    Frontend Refresh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import Footer from './Components/Footer/Footer';
-import FAQ from './Components/FAQ/FAQ';
-import ServiceSection from './Components/Service-Section/ServiceSection';
-import CntForm from './Components/ContactForm/ContactForm';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
+import FAQ from "./Components/FAQ/FAQ";
+import ServiceSection from "./Components/Service-Section/ServiceSection";
+import CntForm from "./Components/ContactForm/ContactForm";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Navigate,
+  useNavigate,
+  useLocation,
 } from "react-router-dom";
 import AboutUs from "./Pages/AboutUs";
 
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
-                <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/about-us" element={<AboutUs />} />
       </Routes>
-      <ServiceSection/>
-      <FAQ/>
-      <Footer/>
-      <CntForm/>
+      <ServiceSection />
+      <FAQ />
+      <Footer />
+      <CntForm />
     </Router>
   );
 }
